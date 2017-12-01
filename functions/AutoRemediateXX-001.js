a51d6cfba82fb19b0480e821d24a3c2770dfdd9f
bretwaldow <bret.waldow@mechanicalrock.io>
2017-12-01 14:36:43 +0800

    Add experimental fake remediation for testing

--- functions/AutoRemediateXX-001.js
+++ functions/AutoRemediateXX-001.js
@@ -6,35 +6,35 @@ const AWS = require("aws-sdk");
 // Dummy remediation test
 module.exports.handler = (event, context, callback) => {
 
-	console.log('S3 BucketPublicReadAccess - Received event:', JSON.stringify(event, null, 2));
+	console.log('Dummy remediation test - Received event:', JSON.stringify(event, null, 2));
 
-		if (!event || !event.resource || event.ruleId !== "S3-001") {
+		if (!event || !event.resource || event.ruleId !== "XX-001") {
 			return handleError("Invalid event");
 		}
-
-    var s3 = new AWS.S3({apiVersion: '2006-03-01'});
-
-    // this code is included to provide a record for debugging
-    var params = {
-      Bucket: event.resource
-    };
-    s3.getBucketAcl(params, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
-    });
-
-    // actually close off access
-    var params = {
-      Bucket: event.resource,
-      ACL: "private",
-    };
-    s3.putBucketAcl(params, function(err, data) {
-      if (err) {
-				console.log(err, err.stack);
-				callback(err, "Failed to auto-remediate S3-001");
-			};
-      else     console.log(data);           // successful response
-    });
+    //
+    // var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+    //
+    // // this code is included to provide a record for debugging
+    // var params = {
+    //   Bucket: event.resource
+    // };
+    // s3.getBucketAcl(params, function(err, data) {
+    //   if (err) console.log(err, err.stack); // an error occurred
+    //   else     console.log(data);           // successful response
+    // });
+    //
+    // // actually close off access
+    // var params = {
+    //   Bucket: event.resource,
+    //   ACL: "private",
+    // };
+    // s3.putBucketAcl(params, function(err, data) {
+    //   if (err) {
+		// 		console.log(err, err.stack);
+		// 		callback(err, "Failed to auto-remediate S3-001");
+		// 	};
+    //   else     console.log(data);           // successful response
+    // });
 
 	callback(null, "Success");
 
