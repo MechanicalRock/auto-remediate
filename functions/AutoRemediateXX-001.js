3eff43552497cacb6940fe4ca8f77b6600b54e01
bretwaldow <bret.waldow@mechanicalrock.io>
2017-12-04 18:59:42 +0800

    Expanded publish to success

--- functions/AutoRemediateXX-001.js
+++ functions/AutoRemediateXX-001.js
@@ -26,7 +26,8 @@ module.exports.handler = (event, context, callback) => {
 		  if (err) {
 			  console.log(err, err.stack); // an error occurred
 				var params = {
-          Message: 'Success message', /* required */
+          Message: 'Error message',
+					Subject: "Error message From AutoRemediateXX-001",
           TopicArn: 'arn:aws:sns:ap-southeast-2:870109809871:AutoRemediationOutcome'
         };
         sns.publish(params, function(err, data) {
@@ -37,6 +38,15 @@ module.exports.handler = (event, context, callback) => {
 
 		  else {
         console.log(data);           // successful response
+				var params = {
+          Message: 'Success message',
+					Subject: "Success message From AutoRemediateXX-001",
+          TopicArn: 'arn:aws:sns:ap-southeast-2:870109809871:AutoRemediationOutcome'
+        };
+        sns.publish(params, function(err, data) {
+          if (err) console.log(err, err.stack); // an error occurred
+          else     console.log(data);           // successful response
+        });
 			}
 
 		});
